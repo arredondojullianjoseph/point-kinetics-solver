e98843906b687e86d12541ec6c78068040135219
Jullian J. Arredondo <arredondojullianjoseph@gmail.com>
2026-08-11 15:10:25 -0400

    Update point kinetics.py
    Added code skeleton and imports for kinetic solver.

--- point kinetics.py
+++ point kinetics.py
@@ -1,3 +1,24 @@
 """
 Six-group point reactor kinetics solver in python, verified against the prompt jump approximation and Inhour equation
 """
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+
+def main():
+    # TODO: 1. Define reactor parameters 
+
+    # TODO: 2. Define the reactivity function 
+
+    # TODO: 3. Set up the system of ODEs
+
+    # TODO: 4. Define initial conditions and time span
+
+    # TODO: 5. Execute solver 
+
+    # TODO: 6. Print the results
+    pass
+
+if __name__ == "__main__":
+    main()
